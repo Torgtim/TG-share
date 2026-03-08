4beed2de5d7f9aae7503f4a2a22a959c325c05f5
Bratos <etorgrimossian@icloud.com>
2026-03-08 14:09:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
-const feed = document.getElementById("feed");
+function shareFile(){
 
-const files = [
- {name:"Example image.png"},
- {name:"Example video.mp4"},
- {name:"Example music.mp3"}
-];
+let name = document.getElementById("username").value;
+let file = document.getElementById("fileInput").files[0];
 
-files.forEach(f=>{
- feed.innerHTML += `
- <div class="card">
-   <p>${f.name}</p>
-   <button>⬇ Download</button>
-   <button>🚩 Report</button>
- </div>
- `;
-});
+if(!name || !file){
+alert("Skriv navn og velg fil");
+return;
+}
+
+let feed = document.getElementById("feed");
+
+let url = URL.createObjectURL(file);
+
+feed.innerHTML =
+`
+<div class="card">
+<p>👤 ${name}</p>
+<p>📁 ${file.name}</p>
+<a href="${url}" target="_blank">🔗 Åpne / Last ned</a>
+</div>
+` + feed.innerHTML;
+
+}
