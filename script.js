35fb015433fb4abd6f1d8d0aa57639d77e66ef62
Bratos <etorgrimossian@icloud.com>
2026-03-08 19:18:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,62 @@
-function shareFile(){
+const supabase = window.supabase.createClient(
+"https://grmuakqenogqjycfhxjy.supabase.co",
+"sb_publishable_Xn2n0aNdbaIyE3_HCyA2Tw_7hdHHwIZ"
+)
 
-let name = document.getElementById("username").value;
-let file = document.getElementById("fileInput").files[0];
+async function upload(){
 
-if(!name || !file){
-alert("Skriv navn og velg fil");
-return;
+let username=document.getElementById("username").value
+let file=document.getElementById("file").files[0]
+
+if(!username || !file){
+alert("Missing username or file")
+return
 }
 
-let feed = document.getElementById("feed");
+let path=Date.now()+"_"+file.name
 
-let url = URL.createObjectURL(file);
+await supabase.storage.from("files").upload(path,file)
 
-feed.innerHTML =
-`
+let {data}=supabase.storage.from("files").getPublicUrl(path)
+
+let url=data.publicUrl
+
+await supabase.from("files").insert([
+{
+username:username,
+filename:file.name,
+fileurl:url
+}
+])
+
+loadFeed()
+
+}
+
+async function loadFeed(){
+
+let {data}=await supabase
+.from("files")
+.select("*")
+.order("created_at",{ascending:false})
+
+let feed=document.getElementById("feed")
+feed.innerHTML=""
+
+data.forEach(file=>{
+
+feed.innerHTML+=`
 <div class="card">
-<p>👤 ${name}</p>
-<p>📁 ${file.name}</p>
-<a href="${url}" target="_blank">🔗 Åpne / Last ned</a>
+<p>👤 ${file.username}</p>
+<p>📁 ${file.filename}</p>
+<a href="${file.fileurl}" target="_blank">Watch</a>
+<br>
+<a href="${file.fileurl}" download>Download</a>
 </div>
-` + feed.innerHTML;
+`
+
+})
 
 }
+
+loadFeed()
